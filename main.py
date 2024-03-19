52ca89e22d7fabfaa6d77d0dc97653950a1aae65
Pieter Meuldijk <logicbug@logicbucks.co.nz>
2024-03-20 03:55:05 +1300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("bye") #TODO: a random todo
+print("bye") #TODO: a random todo 2
+print("bye")
+print("bye")
+first = input("First: ")
+second = input("Second: ")
+sum = float(first)+float(second)
+print("Sum is: "+str(sum))
+# comment: https://www.youtube.com/watch?v=kqtD5dpn9C8
+exec("ls")
+
